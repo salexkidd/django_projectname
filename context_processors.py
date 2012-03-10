50fc884793b69cc21080b37ff1a0dbaf4a55ebed
salexkidd <salexkidd@gmail.com>
2012-03-11 01:13:31 +0900

    Add Basically code

--- context_processors.py
+++ context_processors.py
@@ -2,6 +2,18 @@
 """
 project name valiable to settings.py
 """
+from django.conf import settings
+
+DEFAULT_PROJECT_NAME = "Django Project(Please set settings.PROJECT_NAME)"
+
 
 def project_name(request, *args, **kwargs):
-    return {}
+    """
+    Set project_name to context
+    """
+    project_name = DEFAULT_PROJECT_NAME
+
+    if hasattr(settings, "PROJECT_NAME"):
+        project_name = settings.PROJECT_NAME
+
+    return {'project_name': project_name}
